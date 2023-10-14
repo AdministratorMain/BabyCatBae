0cd46ae0ac2c29714a7acc02fd5f5381cbd8bcc6
AdministratorMain <147939842+AdministratorMain@users.noreply.github.com>
2023-10-14 14:01:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GEQJKX1TN3"></script>
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-GEQJKX1TN3');
+
